1f0e6174f8a46308f6c4259799ebc242ca4d99b4
Donald Chen <donald.chen@rigado.com>
2019-03-20 12:11:26 -0700

    add benchmark

---
+++ linux/hci/hci_test.go
@@ -0,0 +1,21 @@
+package hci
+
+import (
+	"testing"
+
+	"github.com/go-ble/ble/linux/hci/evt"
+)
+
+var r interface{}
+
+func BenchmarkAdv2Map(b *testing.B) {
+	var rr interface{}
+	//ibeacon
+
+	for i := 0; i < b.N; i++ {
+		e := evt.LEAdvertisingReport{2, 1, 3, 1, 144, 17, 101, 210, 60, 246, 30, 2, 1, 2, 26, 255, 76, 0, 2, 21, 255, 254, 45, 18, 30, 75, 15, 164, 153, 78, 4, 99, 49, 239, 205, 171, 52, 18, 120, 86, 195, 205}
+		a := newAdvertisement(e, 0)
+		rr = a.ToMap()
+	}
+	r = rr
+}
