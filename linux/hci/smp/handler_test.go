9f0a542d4f5b3c6a38a09b83f4e1fa47bda9badc
Eric Stutzenberger <eric.stutzenberger@rigado.com>
2021-06-21 11:01:48 -0700

    adjust test name; add test for nominal case

--- linux/hci/smp/handler_test.go
+++ linux/hci/smp/handler_test.go
@@ -1,11 +1,40 @@
 package smp
 
 import (
+	"bytes"
 	"strings"
 	"testing"
 )
 
 func TestOnSmpPairingPublicKey(t *testing.T) {
+	p := pairingContext{}
+	tran := &transport{}
+
+	tran.pairing = &p
+
+	remote, err := GenerateKeys()
+	if err != nil {
+		t.Fatalf("failed to generate remote keys: %v\n", err)
+	}
+	rBytes := MarshalPublicKeyXY(remote.public)
+
+	tran.pairing.scECDHKeys, err = GenerateKeys()
+	if err != nil {
+		t.Fatalf("failed to generate key pair: %v\n", err)
+	}
+
+	_, err = smpOnPairingPublicKey(tran, rBytes)
+	if err != nil {
+		t.Fatalf("failed to process remote public key: %v\n", err)
+	}
+
+	testBytes := MarshalPublicKeyXY(tran.pairing.scRemotePubKey)
+	if !bytes.Equal(rBytes, testBytes) {
+		t.Fatalf("failed to correctly unmarshal remote public key")
+	}
+}
+
+func TestOnSmpPairingPublicKeyMatchingRemoteKey(t *testing.T) {
 	p := pairingContext{}
 	tran := &transport{}
 
